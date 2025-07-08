f2b792972ff442866cc9ecad2cd7894630bae8d6
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-07-08 18:29:11 +0600

    eslint issues fix

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,7 +8,6 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { use } from 'react';
 import { createInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
